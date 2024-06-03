e42388665b67af6894fdd6bc71d030fe7a65bcb8
Sydney Lim <sydney@morpheusdata.com>
2024-06-03 16:16:32 -0700

    implement access token caching

--- src/main/groovy/com/morpheusdata/rubrik/services/GqlApiService.groovy
+++ src/main/groovy/com/morpheusdata/rubrik/services/GqlApiService.groovy
@@ -10,12 +10,14 @@ import org.apache.http.client.utils.URIBuilder
 
 @Slf4j
 class GqlApiService implements PlatformApiServiceInterface {
+	static tokenBuffer = 43200l * 10l
 	@Override
 	Map getAuthConfig(BackupProvider backupProviderModel) {
 		def rtn = [
 			apiUrl: backupProviderModel.serviceUrl,
 			apiVersion: 'v1',
 			token: backupProviderModel.serviceToken,
+			expires: '',
 			gqlPath: '/api/graphql',
 			basePath: '/api'
 		]
@@ -33,28 +35,120 @@ class GqlApiService implements PlatformApiServiceInterface {
 		def rtn = ServiceResponse.prepare()
 		def requestToken = true
 		if(authConfig.token) {
-			rtn.success = true
-			rtn.setData([
-				token: authConfig.token,
-				sessionId: authConfig.sessionId,
-				organizationId: authConfig.organizationId
-			])
-			requestToken = false
+			if(authConfig.expires) {
+				// check if token in authConfig is valid
+				def checkDate = new Date()
+				def tokenValid = ((checkDate.time + tokenBuffer) <= authConfig.expires.time)
+				if (!tokenValid) {
+					requestToken = true
+				} else {
+					requestToken = false
+					rtn.success = true
+					rtn.token = authConfig.token
+					rtn.sessionId = authConfig.sessionId
+					rtn.organizationId = authConfig.organizationId
+				}
+			} else {
+				requestToken = true
+			}
 		}
+
+		// if the token in authConfig is invalid, retrieve cached valid token, or request new token
 		if(requestToken == true) {
-			def apiPath = authConfig.basePath + '/client_token'
-			HttpApiClient.RequestOptions requestOpts = new HttpApiClient.RequestOptions(ignoreSSL: true)
-			ServiceResponse results = HttpApiClient.callJsonApi(authConfig.apiUrl, apiPath, authConfig.username, authConfig.password, requestOpts, 'POST')
-			rtn = results
-			rtn.success = results?.success && results?.error != true
-			if(rtn.success) {
+			def cachedToken = getCachedToken(authConfig.username)
+			if(cachedToken?.token) {
+				rtn.success = true
+				rtn.token = cachedToken.token
+				rtn.expires = cachedToken.expires
+				rtn.sessionId = cachedToken.sessionId
+				rtn.organizationId = cachedToken.organizationId
+			} else {
+				def apiPath = authConfig.basePath + '/client_token'
+				HttpApiClient.RequestOptions requestOpts = new HttpApiClient.RequestOptions(ignoreSSL: true)
+				ServiceResponse results = HttpApiClient.callJsonApi(authConfig.apiUrl, apiPath, authConfig.username, authConfig.password, requestOpts, 'POST')
 				rtn = results
+				rtn.success = results?.success && results?.error != true
+				cacheToken(authConfig.username, authConfig)
+			}
+
+			// update authConfig token
+			if(rtn.success) {
 				authConfig.token = rtn.data.token
 				authConfig.sessionId = rtn.data.sessionId
 				authConfig.organizationId = rtn.data.organizationId
+				authConfig.expires = rtn.data.expires
 			}
 		}
 		return rtn
+	}
+
+	static Thread tokenReaperThread
+	static tokens = [:] // key: client id (username)
+	static tokenLock = new Object()
+
+	// reap expired tokens and return a valid token given the client id/username
+	static getCachedToken(String cacheKey) {
+		def rtn
+		try {
+			synchronized (tokenLock) {
+				if(!tokenReaperThread) {
+					tokenReaperThread = new Thread().start {
+						while(true) {
+							try {
+								sleep(60000L)
+								reapExpiredTokens()
+							} catch(t2) {
+								log.warn("Error Running Rubrik Access Token Reaper Thread ${}", t2)
+							}
+						}
+					}
+				}
+				def cachedToken = tokens[cacheKey]
+				if(cachedToken) {
+					if(cachedToken.expires > new Date(new Date().time) + (10l*60l*1000l)) {
+						rtn = cachedToken
+					}
+
+				}
+			}
+		} catch(Exception ex) {
+			log.error("getCachedToken error: ${}", ex)
+		}
+		return rtn
+	}
+
+	static void cacheToken(String cacheKey, Map authConfig) {
+		try {
+			synchronized(tokenLock) {
+				tokens[cacheKey] = [
+					token: authConfig.token,
+					expires: authConfig.expires,
+					sessionId: authConfig.sessionId,
+					organizationId: authConfig.organizationId
+				]
+			}
+		} catch(Exception ex) {
+			log.error("cacheToken error: ${}", ex)
+		}
+	}
+
+	static reapExpiredTokens() {
+		try {
+			synchronized (tokenLock) {
+				def expiredKeys = []
+				for (cacheKey in tokens.keySet()) {
+					def tokenExpires = tokens[cacheKey]?.expires
+					if(tokenExpires && tokenExpires < new Date(new Date().time - (10l*60l*1000l))) {
+						expiredKeys << cacheKey
+					}
+				}
+				expiredKeys.each { cacheKey ->
+					tokens.remove(cacheKey)
+				}
+			}
+		} catch (Exception ex) {
+			log.error("reapExpiredTokens error: ${}", ex)
+		}
 	}
 
 	@Override
